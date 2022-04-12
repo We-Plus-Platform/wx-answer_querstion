a8ba88ff9d12e6531f89c69495ce9ad0947a0374
zou-wen <2498890383@qq.com>
2022-04-13 00:05:19 +0800

    实现登录,注册请求

--- src/app.config.js
+++ src/app.config.js
@@ -1,11 +1,9 @@
 export default {
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ["pages/login/index", "pages/index/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black"
   }
-}
+};
